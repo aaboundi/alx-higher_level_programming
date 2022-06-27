08fd6bb9238ac2bfcd28f7dfb291ceae19f85303
aaboundi <aadja@gmail.com>
2022-06-27 07:54:49 -0700

    Python8-2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 2-rectangle.py
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, size=0):
+        """Initialize a new square."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
