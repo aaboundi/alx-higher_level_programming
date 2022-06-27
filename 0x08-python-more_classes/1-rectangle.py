4f9bcfd1e178ddaf8e38e446aa67657151314ffa
aaboundi <aadja@gmail.com>
2022-06-27 07:50:42 -0700

    Python8-1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, size):
+        """Initialize a new square."""
+        self.__size = size
