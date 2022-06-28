dc101f097cf37b187bf5df440904978477bd12a4
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-06-28 21:59:49 +0000

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-# 4-print_square.py
-"""
-Module print_square
-Prints a square with the character #.
+# 6-max_integer_test.py
+"""Module to find the max integer in a list
 """
 
 
-def print_square(size):
-    """Prints a square where size is
-    the length and width of the square.
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
     """
-
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
-    for i in range(size):
-        for j in range(size):
-            print('#', end='')
-        print()
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
