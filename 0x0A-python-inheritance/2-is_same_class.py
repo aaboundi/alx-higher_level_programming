2a3d9ed76f925eb0028ed357792545dda41a4db2
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-04 15:49:24 +0000

    Python10-2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,52 +1,15 @@
 #!/usr/bin/python3
-# 2-rectangle.py
-"""Define a class Rectangle."""
+# 2-is_same_class.py
+"""Finds if an object is exactly an instance of a class."""
 
 
-class Rectangle:
-    """Represent a rectangle."""
+def is_same_class(obj, a_class):
+    """Function to determine if obj is an instance of a_class.
+    Args:
+        - obj: object to look at
+        - a_class: class to verify the instance of
+    Returns: True if obj is an instance of a_class,
+    False otherwise
+    """
 
-    def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Get/set the width of the Rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Get/set the height of the Rectangle."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """Return the area of the Rectangle."""
-        return (self.__width * self.__height)
-
-    def perimeter(self):
-        """Return the perimeter of the Rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+    return True if type(obj) is a_class else False
