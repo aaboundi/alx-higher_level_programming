8a0f76fca80427e9161d41b9e8ec52a414dec993
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-04 16:10:09 +0000

    Python10-4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,72 +1,16 @@
 #!/usr/bin/python3
-# 4-rectangle.py
-"""Define a class Rectangle."""
+# 4-inherits_from.py
+"""Finds if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class."""
 
 
-class Rectangle:
-    """Represent a Rectangle."""
+def inherits_from(obj, a_class):
+    """Determines if obj is an instance of a class that
+    inherited from a_class.
+    Args:
+        - obj: object to look at
+        - a_class: class to evaluate
+    Returns: True or False
+    """
 
-    def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """Get/set the width of the Rectangle."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Get/set the height of the Rectangle."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """Return the area of the Rectangle."""
-        return (self.__width * self.__height)
-
-    def perimeter(self):
-        """Return the perimeter of the Rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
-
-    def __str__(self):
-        """Return the printable representation of the Rectangle.
-        Represents the rectangle with the # character.
-        """
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-
-        rect = []
-        for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
-
-    def __repr__(self):
-        """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width)
-        rect += ", " + str(self.__height) + ")"
-        return (rect)
+    return isinstance(obj, a_class) and type(obj) != a_class
