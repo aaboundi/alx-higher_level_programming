a51a4e7053df3af757fc796260bd904a4b27a666
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-13 10:38:04 +0000

    init2

--- 0x0C-python-almost_a_circle/models/__init__.py
+++ 0x0C-python-almost_a_circle/models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # __init__.py
 
-from .base import Base
-from .rectangle import Rectangle
-from .square import Square
+from base import Base
+from rectangle import Rectangle
+from square import Square
