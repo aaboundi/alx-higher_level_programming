d1fb493f45c97d518e50b8d01a7d96312f081c70
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-07-13 10:36:56 +0000

    init1

--- 0x0C-python-almost_a_circle/tests/test_models/__init__.py
+++ 0x0C-python-almost_a_circle/tests/test_models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # __init__.py
 
-from .test_base import *
-from .test_rectangle import *
-from .test_square import *
+from test_base import *
+from test_rectangle import *
+from test_square import *
