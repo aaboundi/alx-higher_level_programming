24a4a80ccae1e4703c8864a32c2ce020205a5571
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-08-10 12:05:02 +0000

    SQL0-1

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
--- lists all databases of my MySQL server
--- lists all databases of my MySQL server
+-- creates the database hbtn_0c_0 of my MySQL server
+-- creates the database hbtn_0c_0 of my MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
