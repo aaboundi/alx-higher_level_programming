9fe4dff308e81063faf7b768d8eb6649b45529fe
aaboundi <aadja@gmail.com>
2022-09-06 08:31:39 -0700

    JS6-test0

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const multiLine = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < 3; i++) {
-	console.log(multiLine[i]);
-}
+const lines = [
+'C is fun',
+'Python is cool',
+'JavaScript is amazing'
+];
+
+for (let i = 0; i < lines.length; i++)
+	console.log(lines[i]);
