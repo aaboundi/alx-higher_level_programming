68d65ab5f8481994a1ee22f61fe7f6b1931aca0b
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-07 15:51:00 +0000

    JS-12

--- 0x12-javascript-warm_up/12-object.js
+++ 0x12-javascript-warm_up/12-object.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-	console.log('Missing number of occurrences');
-}
-else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
-}
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
