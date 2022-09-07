4ae5b8b5193b8a7941470689edbf2d7b7d077ba2
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-07 15:48:13 +0000

    JS-8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-	console.log('Missing number of occurrences');
-}
-else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let square = '';
+    for (let j = 0; j < size; j++) square += 'X';
+    console.log(square);
+  }
 }
