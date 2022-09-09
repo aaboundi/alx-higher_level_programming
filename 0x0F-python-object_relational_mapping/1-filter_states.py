33b48f9c15871d5b09e439ff4dc9e78261e6d492
aaboundi <aadja@gmail.com>
2022-09-09 09:37:50 -0700

    PR-1-correct

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,10 +4,11 @@ N(upper N) from the database hbtn_0e_0_usa"""
 
 """1-filter_states.py"""
 
-import MySQLdb
-import sys
-
 if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
