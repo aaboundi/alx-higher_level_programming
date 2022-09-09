de3479798331d27ae977c3622af610fc0edb6be7
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-09 16:31:06 +0000

    PR-1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N(upper N) from the database hbtn_0e_0_usa"""
+"""lists all states with a name starting with
+N(upper N) from the database hbtn_0e_0_usa"""
 
 """1-filter_states.py"""
 
@@ -10,5 +11,5 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY `id`;")
+    cur.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in cur.fetchall() if state[1][0] == "N"]
