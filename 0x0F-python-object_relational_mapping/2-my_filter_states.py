abc5a4440bf5738b06fcbbb5bc79d0d64581b795
aaboundi <aadja@gmail.com>
2022-09-09 09:40:47 -0700

    PR-2-correct

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,12 +8,12 @@ where name matches the argument
 """2-my_filter_states.py"""
 
 if __name__ == '__main__':
-    
+
     import MySQLdb
     import sys
 
     db = MySQLdb.connect(host='localhost', port=3306,
-            user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
