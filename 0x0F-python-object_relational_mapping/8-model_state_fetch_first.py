1b41793c76c77db766cc3800303db17e894b7c42
aaboundi <aadja@gmail.com>
2022-09-10 09:54:14 -0700

    PR-8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa.
+"""
+
+"""8-model_state_fetch_first.py"""
+
+if __name__ == '__main__':
+
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    first = session.query(State).order_by(State.id).first()
+    if first:
+        print("{}: {}".format(first.id, first.name))
+    else:
+        print("Nothing")
+    session.close()
