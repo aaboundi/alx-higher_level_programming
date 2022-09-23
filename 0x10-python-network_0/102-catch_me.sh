dc5bd84c246ea8fd147262de05e27dabe8f042e8
aaboundi <99649590+aaboundi@users.noreply.github.com>
2022-09-23 22:59:27 +0000

    done

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Follows a URL for it to respond with "You got me!"
-curl -s 0.0.0.0:5000/catch_me -X PUT -L -d "user_id=98" -H "You got me!"
+curl -s 0.0.0.0:5000/catch_me -X PUT -L -d "user_id=98" -H "Hello School!"
